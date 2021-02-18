6848f1151e05d3bb6a8a6826f8817006b3cd843a
Yan Felix <devyanfelix@gmail.com>
2021-02-18 02:48:27 -0300

    Service layer, component registration

--- src/main/java/com/br/yan/repositories/UserRepository.java
+++ src/main/java/com/br/yan/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.br.yan.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.br.yan.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
---
+++ src/main/java/com/br/yan/services/UserService.java
@@ -0,0 +1,27 @@
+package com.br.yan.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.br.yan.entities.User;
+import com.br.yan.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id){
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
