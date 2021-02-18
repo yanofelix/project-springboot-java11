407109cdaa4c7dbd443d77d141b6f88a9e2b07b6
Yan Felix <devyanfelix@gmail.com>
2021-02-17 22:16:28 -0300

    first commit

---
+++ src/main/java/com/br/yan/CursoNelioApplication.java
@@ -0,0 +1,13 @@
+package com.br.yan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoNelioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoNelioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/yan/CursoNelioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.yan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoNelioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
