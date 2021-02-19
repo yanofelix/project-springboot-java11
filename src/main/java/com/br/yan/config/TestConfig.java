fd130a80b8b2c33f407cd8b977a131f56339fac9
Yan Felix <devyanfelix@gmail.com>
2021-02-19 16:35:24 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/br/yan/config/TestConfig.java
+++ src/main/java/com/br/yan/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.br.yan.entities.Category;
 import com.br.yan.entities.Order;
+import com.br.yan.entities.Product;
 import com.br.yan.entities.User;
 import com.br.yan.entities.enums.OrderStatus;
 import com.br.yan.repositories.CategoryRepository;
 import com.br.yan.repositories.OrderRepository;
+import com.br.yan.repositories.ProductRepository;
 import com.br.yan.repositories.UserRepository;
 
 @Configuration
@@ -26,6 +28,9 @@ public class TestConfig implements CommandLineRunner {
 	private OrderRepository orderRepository;
 	@Autowired
 	private CategoryRepository categoryRepository;
+	@Autowired
+	private ProductRepository productRepository;
+	
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -35,7 +40,23 @@ public class TestConfig implements CommandLineRunner {
 		Category cat2 = new Category(null, "Books");
 		Category cat3 = new Category(null, "Computers");
 		
+		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+		
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4 ,p5));
+		
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
+		
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
